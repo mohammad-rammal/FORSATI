edca521e42a66987417545ba185832a8b2ef91aa
mohammad-rammal <mohammad.rammal@hotmail.com>
2024-08-16 00:02:07 +0300

    change

--- script.js
+++ script.js
@@ -2,3 +2,24 @@ function toggleMenu() {
     const navLinks = document.getElementById('nav-links');
     navLinks.classList.toggle('show');
 }
+
+var modal = document.getElementById("popup-modal");
+
+var link = document.querySelector('.box[href="www.e5.com"]');
+
+var span = document.getElementsByClassName("close")[0];
+
+link.onclick = function(event) {
+    event.preventDefault();
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
