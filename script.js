2479dd19b71aecca09755c84e7bef2e0b0fda85c
mohammad-rammal <mohammad.rammal@hotmail.com>
2024-08-17 01:14:15 +0300

    chamge

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ linkE3.onclick = function(event) {
     event.preventDefault(); 
     window.location.href = "https://res-console.cloudinary.com/dftxzx2zc/media_explorer_thumbnails/7a0838cca550f345f6ab0879a8dc03ee/download";
 };
+
 linkE4.onclick = function(event) {
     event.preventDefault(); 
     openModal("https://res.cloudinary.com/dftxzx2zc/image/upload/v1723845728/Extra/lnbf81nbmfeixs26qg49.png", "image");
