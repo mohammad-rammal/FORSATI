2b138ca5e8f3ff46099828925286e5c3cab92dc0
mohammad-rammal <mohammad.rammal@hotmail.com>
2024-08-17 01:11:44 +0300

    change

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let modal = document.getElementById("popup-modal");
 
+let linkE1 = document.querySelector('.box[href="www.e1.com"]');
 let linkE2 = document.querySelector('.box[href="www.e2.com"]');
 let linkE3 = document.querySelector('.box[href="www.e3.com"]');
+let linkE4 = document.querySelector('.box[href="www.e4.com"]');
 let linkE5 = document.querySelector('.box[href="www.e5.com"]');
 
 let span = document.getElementsByClassName("close")[0];
@@ -22,19 +24,27 @@ function openModal(contentSrc, type) {
     modal.style.display = "block";
 }
 
+linkE1.onclick = function(event) {
+    event.preventDefault(); 
+    window.location.href = "https://res-console.cloudinary.com/dftxzx2zc/media_explorer_thumbnails/82cfa24b27ceaade062b88575b3e8ab1/download";
+};
 
 linkE2.onclick = function(event) {
     event.preventDefault(); 
-    openModal("https://res.cloudinary.com/dftxzx2zc/image/upload/v1723839180/Images/txjp6eo3wcqgizosbjen.png", "image");
+    openModal("https://res.cloudinary.com/dftxzx2zc/image/upload/v1723845580/Extra/kbtllozarauzdq57e6zs.png", "image");
 }
 
 linkE3.onclick = function(event) {
     event.preventDefault(); 
-    window.location.href = "https://res-console.cloudinary.com/dftxzx2zc/media_explorer_thumbnails/5159f1d7e6fbd0204781a265edf67b9e/download";
+    window.location.href = "https://res-console.cloudinary.com/dftxzx2zc/media_explorer_thumbnails/7a0838cca550f345f6ab0879a8dc03ee/download";
 };
+linkE4.onclick = function(event) {
+    event.preventDefault(); 
+    openModal("https://res.cloudinary.com/dftxzx2zc/image/upload/v1723845728/Extra/lnbf81nbmfeixs26qg49.png", "image");
+}
 linkE5.onclick = function(event) {
     event.preventDefault(); 
-    openModal("https://res.cloudinary.com/dftxzx2zc/image/upload/v1723755040/Images/pi8do8yfhi6outzi9w2f.png", "image");
+    openModal("https://res.cloudinary.com/dftxzx2zc/image/upload/v1723845337/Extra/dwxqyq7yzz6bgrz1afhz.png", "image");
 }
 
 span.onclick = function() {
